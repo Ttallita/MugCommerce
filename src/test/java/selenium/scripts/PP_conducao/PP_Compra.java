a35afdcdfeccf1fb054874bbc834fea9a62438a2
Hitagi71 <andre.gomes7110@gmail.com>
2022-11-20 17:36:25 -0300

    iniciando dashboard com um produto

--- src/test/java/selenium/scripts/PP_conducao/PP_Compra.java
+++ src/test/java/selenium/scripts/PP_conducao/PP_Compra.java
@@ -1,7 +1,24 @@
 package selenium.scripts.PP_conducao;
 
-public class PP_Compra {
+import org.junit.jupiter.api.Test;
+import selenium.pageModels.components.HeaderClienteComponent;
+import selenium.scripts.TesteAbstract;
+import selenium.service.TesteCompraService;
 
+public class PP_Compra extends TesteAbstract {
+
+    @Override
+    protected void configurarCenarioTeste() throws InterruptedException {}
+    private HeaderClienteComponent headerCliente;
+
+    @Test
+    public void realizaCompra() throws InterruptedException {
+        headerCliente = (HeaderClienteComponent) this.realizarLoginClientePadrao().getHeader(driver);
+
+        for(int i = 0; i < 499; i++) {
+            TesteCompraService.realizarCompra(driver, headerCliente);
+        }
+    }
 
 
 }
